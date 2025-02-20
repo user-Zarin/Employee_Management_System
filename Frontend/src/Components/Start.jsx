9ec44fbeca87f007dd9ce347e078b8c69afe7c1b
user-Zarin <155997992+user-Zarin@users.noreply.github.com>
2025-02-20 19:45:15 +0530

    Update Start.jsx

--- Frontend/src/Components/Start.jsx
+++ Frontend/src/Components/Start.jsx
@@ -7,7 +7,7 @@ const Start = () => {
     const navigate = useNavigate()
   axios.defaults.withCredentials = true;
   useEffect(() => {
-    axios.get('http://localhost:3000/verify')
+    axios.get('https://employee-management-system-qx5m.onrender.com/verify')
     .then(result => {
       if(result.data.Status) {
         if(result.data.role === "admin") {
