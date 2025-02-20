cf1a5056e5311c5831923cb2b1901e5c4f2fcd9b
user-Zarin <155997992+user-Zarin@users.noreply.github.com>
2025-02-20 19:46:45 +0530

    Update EditEmployee.jsx

--- Frontend/src/Components/EditEmployee.jsx
+++ Frontend/src/Components/EditEmployee.jsx
@@ -24,7 +24,7 @@ const EditEmployee = () => {
             }
         }).catch(err => console.log(err))
 
-        axios.get('http://localhost:3000/auth/employee/'+id)
+        axios.get('https://employee-management-system-qx5m.onrender.com/auth/employee/'+id)
         .then(result => {
             setEmployee({
                 ...employee,
@@ -39,7 +39,7 @@ const EditEmployee = () => {
 
     const handleSubmit = (e) => {
         e.preventDefault()
-        axios.put('http://localhost:3000/auth/edit_employee/'+id, employee)
+        axios.put('https://employee-management-system-qx5m.onrender.com/auth/edit_employee/'+id, employee)
         .then(result => {
             if(result.data.Status) {
                 navigate('/dashboard/employee')
