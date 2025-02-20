c7612b524e2d28ea807e1e33cfb516b94bbca456
user-Zarin <155997992+user-Zarin@users.noreply.github.com>
2025-02-20 19:44:50 +0530

    Update Login.jsx

--- Frontend/src/Components/Login.jsx
+++ Frontend/src/Components/Login.jsx
@@ -14,7 +14,7 @@ const Login = () => {
     axios.defaults.withCredentials = true;
     const handleSubmit = (event) => {
         event.preventDefault()
-        axios.post('http://localhost:3000/auth/adminlogin', values)
+        axios.post('https://employee-management-system-qx5m.onrender.com/auth/adminlogin', values)
         .then(result => {
             if(result.data.loginStatus) {
                 localStorage.setItem("valid", true)
